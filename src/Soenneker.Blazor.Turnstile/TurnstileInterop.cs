281b34a8f3d83d0de79aba7338996dc1df875222
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-03-22 15:05:44 -0500

    More efficient module loading

--- src/Soenneker.Blazor.Turnstile/TurnstileInterop.cs
+++ src/Soenneker.Blazor.Turnstile/TurnstileInterop.cs
@@ -35,7 +35,7 @@ public sealed class TurnstileInterop : ITurnstileInterop
     {
         await _resourceLoader.LoadScriptAndWaitForVariable("https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit", "turnstile", cancellationToken: token);
 
-        await _resourceLoader.ImportModuleAndWaitUntilAvailable(_module, _moduleName, 100, token);
+        await _resourceLoader.ImportModule(_module, token);
     }
 
     public async ValueTask Initialize(CancellationToken cancellationToken = default)
